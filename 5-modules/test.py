e9fe14b57f290032fb57b7aa2773c9d5dd0948ff
krishnaraj <krish0195@gmail.com>
2024-10-06 19:09:00 +0530

    test

---
+++ 5-modules/test.py
@@ -0,0 +1,10 @@
+from package.maths import *
+
+print(addition(5,2))
+print(subtraction(5,3))
+
+def addition(a,b):
+    return a+b
+
+from package.subpackage.mult import multiply
+print(multiply(5,3))
