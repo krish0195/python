6a960cef69bb782658eb65f90e23ad1613b02300
krishnaraj <krish0195@gmail.com>
2024-10-18 17:44:32 +0530

    form and submit

--- 13-flask/flask/getpost.py
+++ 13-flask/flask/getpost.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 '''
  It creates an instance of the Flask class, 
  which will be your WSGI (Web Server Gateway Interface) application.
@@ -10,13 +10,27 @@ app=Flask(__name__)
 def welcome():
     return "<html><H1>Welcome to the flask course</H1></html>"
 
-@app.route("/index",method=['GET'])
+@app.route("/index",methods=['GET'])
 def index():
     return render_template('index.html')
 
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/form',methods=['GET','POST'])
+def form():
+    if request.method=='POST':
+        name=request.form['name']
+        return f'Hello {name}!'
+    return render_template('form.html')
+
+@app.route('/submit',methods=['GET','POST'])
+def submit():
+    if request.method=='POST':
+        name=request.form['name']
+        return f'Hello {name}!'
+    return render_template('form.html')
 
 
 if __name__=="__main__":
